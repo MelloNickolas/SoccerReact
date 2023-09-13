e02250751ce09bcc4143042302095fc3692ddccf
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2023-09-13 19:13:38 -0300

    add input

--- soccer/src/pages/Toolbar/toolbar.tsx
+++ soccer/src/pages/Toolbar/toolbar.tsx
@@ -1,7 +1,25 @@
+import styles from "../Toolbar/Toolbar.module.scss";
+import logo from "../../assets/logo.png";
+import { BiSearch } from "react-icons/bi";
+
 export default function Toolbar(){
     return(
-        <header>
-                <div>kfdsjghlak</div>
+        <header className={styles.toolbar}>
+
+        <div className={styles.start}> 
+            <img src={logo} alt="logo" className={styles.logo}/>
+        </div>
+
+        <div className={styles.middle}> 
+        <div className={styles.containerInput}>
+        <BiSearch className={styles.icon} size={25}/>
+         <input className={styles.input}/>
+        </div>
+        </div>
+
+
+        <div className={styles.end}> afsdfd </div>
+
         </header>
     )
 }
