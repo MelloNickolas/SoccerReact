682f87771484678a017334155140c39a0289bca4
MelloNickolas <nickolasphmello10@gmail.com>
2023-09-20 08:03:29 -0300

    Update with filters

--- soccer/src/pages/Toolbar/toolbar.tsx
+++ soccer/src/pages/Toolbar/toolbar.tsx
@@ -1,25 +1,32 @@
 import styles from "../Toolbar/Toolbar.module.scss";
 import logo from "../../assets/logo.png";
 import { BiSearch } from "react-icons/bi";
+import filtros from "./filtros.json"
 
-export default function Toolbar(){
-    return(
-        <header className={styles.toolbar}>
+type IOpcao = typeof filtros[0];
 
-        <div className={styles.start}> 
-            <img src={logo} alt="logo" className={styles.logo}/>
-        </div>
+export default function Toolbar() {
+  return (
+    <header className={styles.toolbar}>
+      <div className={styles.start}>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </div>
 
-        <div className={styles.middle}> 
+      <div className={styles.middle}>
         <div className={styles.containerInput}>
-        <BiSearch className={styles.icon} size={25}/>
-         <input className={styles.input}/>
+          <BiSearch className={styles.icon} size={25} />
+          <input className={styles.input} />
         </div>
+      </div>
+
+      <div className={styles.end}>
+        <div className={styles.end__box}>
+            {filtros.map((opcao) => 
+                <button className={styles.end__box__button} key={opcao.id}> {opcao.label} </button>
+            
+            )}
         </div>
-
-
-        <div className={styles.end}> afsdfd </div>
-
-        </header>
-    )
+      </div>
+    </header>
+  );
 }
